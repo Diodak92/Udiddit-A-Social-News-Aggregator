e1d38bf3a9e8b77d89e8c82761171e5bd02dc07b
Diodak92 <tomasz.kowalski92@icloud.com>
2022-10-16 19:56:34 +0200

    Added CHECK constraints

--- udiddit_DDL.sql
+++ udiddit_DDL.sql
@@ -1,7 +1,8 @@
 -- create users table
 CREATE TABLE "users"(
     "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(25) UNIQUE NOT NULL
+    "username" VARCHAR(25) UNIQUE NOT NULL,
+    CONSTRAINT empty_user CHECK(LENGTH(TRIM("username")) > 0)
 );
 
 -- create topics table
@@ -14,22 +15,22 @@ CREATE TABLE "topics"(
 -- create posts table
 CREATE TABLE "posts"(
     "id" SERIAL PRIMARY KEY,
-    "title" TEXT(100) NOT NULL
-    "url" VARCHAR(2048) DEFAULT NULL
-    "text_content" TEXT DEFAULT NULL
+    "title" VARCHAR(100) NOT NULL,
+    "url" VARCHAR(2048) DEFAULT NULL,
+    "text_content" TEXT DEFAULT NULL,
+    CONSTRAINT "url_or_text" CHECK(("url" IS NOT NULL AND "text_content" IS NULL) OR
+    ("url" IS NULL AND "text_content" IS NOT NULL))
 );
 
 CREATE TABLE "comments"(
     "id" SERIAL PRIMARY KEY,
-    "text" TEXT NOT NULL,
-
+    "text" TEXT NOT NULL
 );
 
 CREATE TABLE "votes"(
     "id" SERIAL PRIMARY KEY,
     "value" SMALLINT,
     "user_id" INTEGER,
-    "post_id" INTEGER
-)
-ADD CONSTRAINT up_down_vote CHECK("value" = -1 OR "value" = 1)
-;
+    "post_id" INTEGER,
+    CONSTRAINT up_down_vote CHECK("value" = -1 OR "value" = 1)
+);
