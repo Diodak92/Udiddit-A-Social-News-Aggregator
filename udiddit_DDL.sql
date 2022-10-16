834db69d2bdc745e2f3d1592e49933a0bab4e5a4
Diodak92 <tomasz.kowalski92@icloud.com>
2022-10-16 15:23:23 +0200

    Created all five tables for new schema

--- udiddit_DDL.sql
+++ udiddit_DDL.sql
@@ -1,20 +1,35 @@
 -- create users table
 CREATE TABLE "users"(
+    "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(25) UNIQUE NOT NULL
 );
 
 -- create topics table
 CREATE TABLE "topics"(
+    "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(30) UNIQUE NOT NULL,
-    "description" TEXT(500)
+    "description" VARCHAR(500) DEFAULT NULL
 );
 
 -- create posts table
 CREATE TABLE "posts"(
+    "id" SERIAL PRIMARY KEY,
     "title" TEXT(100) NOT NULL
+    "url" VARCHAR(2048) DEFAULT NULL
+    "text_content" TEXT DEFAULT NULL
 );
 
 CREATE TABLE "comments"(
+    "id" SERIAL PRIMARY KEY,
     "text" TEXT NOT NULL,
 
 );
+
+CREATE TABLE "votes"(
+    "id" SERIAL PRIMARY KEY,
+    "value" SMALLINT,
+    "user_id" INTEGER,
+    "post_id" INTEGER
+)
+ADD CONSTRAINT up_down_vote CHECK("value" = -1 OR "value" = 1)
+;
