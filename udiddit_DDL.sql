7f94def36960afe8e228f7b860960802bf9dcebc
Kowalski <tomaszmarcin.kowalski@refinitiv.com>
2022-10-20 12:19:37 +0200

    Addes foreign key constraint to posts and votes table

--- udiddit_DDL.sql
+++ udiddit_DDL.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "users"(
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR(25) UNIQUE NOT NULL,
-    CONSTRAINT empty_user CHECK(LENGTH(TRIM("username")) > 0)
+    CONSTRAINT EMPTY_USER CHECK(LENGTH(TRIM("username")) > 0)
 );
 
 -- create topics table
@@ -18,8 +18,11 @@ CREATE TABLE "posts"(
     "title" VARCHAR(100) NOT NULL,
     "url" VARCHAR(2048) DEFAULT NULL,
     "text_content" TEXT DEFAULT NULL,
-    CONSTRAINT "url_or_text" CHECK(("url" IS NOT NULL AND "text_content" IS NULL) OR
-    ("url" IS NULL AND "text_content" IS NOT NULL))
+    "topic_id" INTEGER,
+    "user_id" INTEGER,
+    CONSTRAINT "url_or_text" CHECK(("url" IS NOT NULL AND "text_content" IS NULL) OR ("url" IS NULL AND "text_content" IS NOT NULL)),
+    CONSTRAINT "delete_posts" FOREIGN KEY ("topic_id") REFERENCES "topics" ON DELETE CASCADE,
+    CONSTRAINT "dissociate_post" FOREIGN KEY ("user_id") REFERENCES "users" ON DELETE SET NULL
 );
 
 CREATE TABLE "comments"(
@@ -32,5 +35,7 @@ CREATE TABLE "votes"(
     "value" SMALLINT,
     "user_id" INTEGER,
     "post_id" INTEGER,
-    CONSTRAINT up_down_vote CHECK("value" = -1 OR "value" = 1)
+    CONSTRAINT UP_DOWN_VOTE CHECK("value" = -1 OR "value" = 1),
+    CONSTRAINT "dissociate_user" FOREIGN KEY ("user_id") REFERENCES "users" ON DELETE SET NULL,
+    CONSTRAINT "delete_votes" FOREIGN KEY ("post_id") REFERENCES "posts" ON DELETE CASCADE
 );
