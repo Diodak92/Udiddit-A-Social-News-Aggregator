a9f1b8300e8cf071424995b452f6497db66b460a
Diodak92 <tomasz.kowalski92@icloud.com>
2022-10-21 19:18:00 +0200

    Updated script for automatic database creation

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,12 @@
+# force delete database if exist
+psql -d postgres -c 'DROP DATABASE udiddit WITH (FORCE);'
 # create new database
 psql -d postgres -c 'CREATE DATABASE udiddit;'
 # connect to new db, create tables and insert data
 psql -d udiddit -f bad-db.sql
+# create new database schema
+psql -d udiddit -f udiddit_DDL.sql
 # explore tables schema
 psql udiddit -c '\dt'
+# connect to database
+psql udiddit
