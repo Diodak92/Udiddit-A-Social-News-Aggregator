a37d566149d79fca06e739fc002c5f3ab1f23b12
Diodak92 <tomasz.kowalski92@icloud.com>
2022-10-22 21:50:11 +0200

    Delete tables after data migration

--- create_db.sh
+++ create_db.sh
@@ -2,11 +2,16 @@
 psql -d postgres -c 'DROP DATABASE udiddit WITH (FORCE);'
 # create new database
 psql -d postgres -c 'CREATE DATABASE udiddit;'
-# connect to new db, create tables and insert data
+# import data from bad db
 psql -d udiddit -f bad-db.sql
 # create new database schema
 psql -d udiddit -f udiddit_DDL.sql
+# migrate data
+psql -d udiddit -f udiddit_DML.sql
 # explore tables schema
 psql udiddit -c '\dt'
 # connect to database
 psql udiddit
+# delete tables after data migration
+psql -d postgres -c 'DROP TABLE bad_comments;'
+psql -d postgres -c 'DROP TABLE bad_posts;'
