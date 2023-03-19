4d328a6eae44b03a8e6d7b4f47e9dafa7f7215f0
Diodak92 <tomasz.kowalski92@icloud.com>
2023-03-19 01:33:50 +0100

    Added queries for finding latest posts by given user or topic

--- udiddit_DQL.sql
+++ udiddit_DQL.sql
@@ -1,11 +1,11 @@
--- a
+-- a.
 -- List all users who haven't logged in the last year
 
 SELECT *
 FROM users
 WHERE last_login <= (CURRENT_DATE - interval '1 year');
 
--- b
+-- b.
 -- List all users who haven’t created any post
 
 SELECT DISTINCT id, username 
@@ -16,13 +16,13 @@ FROM
      WHERE p.id IS NULL
      ORDER BY u.id) inactive_users;
 
--- c
+-- c.
 -- Find a user by their username
 SELECT *
 FROM users
 WHERE username = 'Angelo_Erdman';
 
--- d
+-- d.
 -- List all topics that don’t have any posts
 SELECT topics.id AS topic_id, topics.name AS topic_name
 FROM topics
@@ -31,21 +31,50 @@ ON posts.topic_id = topics.id
 WHERE posts.topic_id IS NULL
 ORDER BY topic_id;
 
--- e
+-- e.
 -- Find a topic by its name
 SELECT *
 FROM topics
 WHERE name LIKE 'engine%';
 
+-- f.
+-- List the latest 20 posts for a given topic
+SELECT p.id AS post_id, p.title AS post_title, t.name AS topic_name,
+p.created_on
+FROM posts p
+JOIN topics t
+ON p.topic_id = t.id
+WHERE t.name = 'Plastic'
+ORDER BY created_on DESC
+LIMIT 20;
 
--- h
+-- g.
+-- List the latest 20 posts made by a given user
+SELECT u.id AS user_id, u.username, 
+p.id AS post_id, p.title AS post_title, p.created_on
+FROM posts p
+JOIN users u
+ON p.user_id = u.id
+WHERE u.username = 'Luz45'
+ORDER BY created_on DESC
+LIMIT 20;
+
+-- h.
 -- Find all posts that link to a specific URL, for moderation purposes
 
 SELECT *
 FROM posts
 WHERE url = 'http://shakira.com'; -- IS NOT NULL
 
--- k
+
+-- i.
+-- List all the top-level comments (those that don’t have a parent comment) for a given post.
+
+-- j.
+-- List all the direct children of a parent comment.
+
+
+-- k.
 -- List the latest 20 comments made by a given user
 
 SELECT users.id AS user_id, users.username, 
@@ -58,7 +87,7 @@ WHERE username = 'Monica_Weber85'
 ORDER BY created_on DESC
 LIMIT 20;
 
--- l
+-- l.
 -- Compute the score of a post, defined as the difference between the number of upvotes and the number of downvotes
 SELECT SUM(value) AS post_score
 FROM votes
